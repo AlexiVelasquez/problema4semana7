5b54c8466b9f1f5783f978415a4feda46b586731
alexi <alexi@LAPTOP-K1LI30UL>
2023-10-05 00:14:17 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace problema4semana7  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Ingresa un número: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    break; 
+                }
+
+                for (int i = 0; i < numero; i++)
+                {
+                    for (int j = 0; j < numero; j++)
+                    {
+                        
+                        Console.Write("**");
+
+                    }
+                    Console.WriteLine(); 
+                    
+                }
+            }   Console.ReadKey();  
+        }
+    }
+}
+
